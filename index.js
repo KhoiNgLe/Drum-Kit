c521da7f72ea3cd0500dddb9c8cad9bddcf045d5
KhoiNgLe <91035280+KhoiNgLe@users.noreply.github.com>
2021-10-17 11:55:47 -0400

    Update index.js file
    Completed

--- index.js
+++ index.js
@@ -1,75 +1,81 @@
-var numberOfDrumButtons = document.querySelectorAll(".drum").length
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for( var i = 0; i<numberOfDrumButtons; i++){
+for (var i = 0; i < numberOfDrumButtons; i++) {
 
-  document.querySelectorAll(".drum")[i].addEventListener("click", function());
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
 
-      var buttonInnerHTML = this.innterHTML
+    var buttonInnerHTML = this.innerHTML;
 
-      makeSound(buttonInnerHTML);
+    makeSound(buttonInnerHTML);
 
-      buttonAnimation(buttonInnerHTML);
-    });
+    buttonAnimation(buttonInnerHTML);
 
+  });
 
-document.addEventListener("keypress",function(event)){
+}
+
+document.addEventListener("keypress", function(event) {
+
   makeSound(event.key);
 
   buttonAnimation(event.key);
 
 });
-    function makeSound(key){
 
 
-      switch(key){
-        case "w":
-        var tom1 = new Audio("C:\Users\16139\OneDrive\Desktop\Web Development\Drum Kit\sounds\tom-1.mp3");
-        tom1.play();
-        break;
+function makeSound(key) {
 
-        case "a":
-        var tom2 = new Audio("C:\Users\16139\OneDrive\Desktop\Web Development\Drum Kit\sounds\tom-2.mp3");
-        tom2.play();
-          break;
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
 
-        case "s":
-        var tom3 = new Audio("C:\Users\16139\OneDrive\Desktop\Web Development\Drum Kit\sounds\tom-3.mp3");
-        tom3.play();
-        break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
 
-        case "d":
-        var tom4 = new Audio("C:\Users\16139\OneDrive\Desktop\Web Development\Drum Kit\sounds\tom-4.mp3");
-        tom4.play();
-        break;
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
 
-        case "j":
-        var kickBass = new Audio("C:\Users\16139\OneDrive\Desktop\Web Development\Drum Kit\sounds\kick-bass.mp3");
-        kickBass.play();
-        break;
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
 
-        case "k":
-        var snare = new Audio("C:\Users\16139\OneDrive\Desktop\Web Development\Drum Kit\sounds\snare.mp3");
-        snare.play();
-        break;
+    case "j":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
 
-        case "l":
-        var tom6 = new Audio("C:\Users\16139\OneDrive\Desktop\Web Development\Drum Kit\sounds\kick-bass.mp3");
-        tom6.play();
-        break;
+    case "k":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
 
-        default: console.log (buttonInnerHTML);
-      }
-  })
+    case "l":
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
 
-}  }
+
+    default: console.log(key);
+
+  }
+}
+
 
 function buttonAnimation(currentKey) {
+
   var activeButton = document.querySelector("." + currentKey);
 
   activeButton.classList.add("pressed");
 
-  setTimeout (functiuon(){
+  setTimeout(function() {
     activeButton.classList.remove("pressed");
   }, 100);
-  })
+
 }
